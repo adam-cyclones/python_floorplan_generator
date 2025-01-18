39e78f91d540b652bff6089db55d2b08e4450139
Adam Crockett <mr.adam.crockett@gmail.com>
2025-01-18 23:00:32 +0000

    Rooms now declare type only and size and position are set later on. some changes also add the planned lookup for room types from a dataset

--- lib/data/Room.py
+++ lib/data/Room.py
@@ -1,16 +1,34 @@
 from lib.store import store
 from lib.data.Size import Size
 from lib.data.Metadata import Metadata
+from lib.data.Position import Position
 
-
+def format_id(id: str):
+    return id.lower().replace(" ", "_")
 class Room:
-    size: Size
     metadata: Metadata
+    position: Position
+    type: str
+    size: Size
 
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, type: str) -> None:
         count = store.incr_room()
-        self.metadata = Metadata(id=f"room_{count}", human_name=f"Room {count}")
-        self.size = Size(width, height)
+        self.metadata = Metadata(id=format_id(f"room_{type}_{count}"), human_name=f"Room: {type} ({count})")
+        self.position = Position(0, 0)
+        self.size = Size(0, 0)
+        
+        if self.validate_type(type):
+            self.type = type
+        else:
+            raise ValueError("Invalid room type")
 
-    def rename(self, new_name: str):
-        self.name = new_name
+    def validate_type(self, type: str):
+        # Todo: query database for type
+        return True
+
+    def set_size(self, width: int, height: int):
+        self.size.width = width
+        self.size.height = height
+    def set_position(self, x: int, y: int):
+        self.position.x = x
+        self.position.y = y
--- test/room_test.py
+++ test/room_test.py
@@ -3,7 +3,13 @@ from lib.data.Room import Room
 
 
 def test_room_has_width_and_height():
-    room = Room(100, 100)
+    room = Room(100, 100, 0, 0)
 
     assert room.size.width == 100
     assert room.size.height == 100
+
+def test_room_can_be_positioned():
+    room = Room(100, 100, 10, 10)
+
+    assert room.position.x == 10
+    assert room.position.y == 10
